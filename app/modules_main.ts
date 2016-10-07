94650fc3242c81b8d5ef9e120b8cc1b3fba0491f
Pricilla Tan <pricillatsf@gmail.com>
2016-10-07 10:04:45 +0000

    learning angular2

---
+++ app/modules_main.ts
@@ -0,0 +1,5 @@
+
+import {bootstrap} from "angular2/platform/browser"     //importing bootstrap function
+import {MyModulesClass} from "./modules_app.component"  //importing component function
+
+bootstrap(MyModulesClass);
